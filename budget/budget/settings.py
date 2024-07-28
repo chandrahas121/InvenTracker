2a1f8d78a4830d44ac2d96b2688a701a18e379fa
chandrahas121 <chandrahas7c@gmail.com>
2024-07-28 09:12:38 +0530

    zense-project-2

--- budget/budget/settings.py
+++ budget/budget/settings.py
@@ -137,6 +137,14 @@ LOGIN_REDIRECT_URL = 'dashboard-index'
 
 LOGIN_URL = 'user-login'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS =True
+EMAIL_HOST_USER = 'chandrahas7c@gmail.com'
+EMAIL_HOST_PASSWORD = 'Chandu@123'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
