35de11c4d8b6356c1b1a3118923a16738dc7c55b
chandrahas121 <chandrahas7c@gmail.com>
2024-08-03 21:06:55 +0530

    changes

--- InvenTracker/a_rtchat/views.py
+++ InvenTracker/a_rtchat/views.py
@@ -1,25 +1,10 @@
-from django.shortcuts import render,get_object_or_404,redirect
-from .models import *
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-
 from django.urls import reverse
+from .models import ChatRoom, Message
+from django.contrib.auth.models import User
+from django.db.models import Q 
 
- # @login_required
-# def chat_view(request):
-#   chat_group = get_object_or_404(ChatGroup,group_name="public-chat")
-#   chat_messages = chat_group.chat_messages.all()[:30]
-#   form = ChatmessageCreateForm()
-
-#   if request.method == 'POST':
-#     form = ChatmessageCreateForm(request.POST)
-#     if form.is_valid:
-#       message = form.save(commit=False)
-#       message.author = request.user
-#       message.group = chat_group
-#       message.save()
-#       return redirect('home')
-
-#   return render(request,'a_rtchat/chat.html',{'chat_messages':chat_messages,'form':form})
 @login_required
 def create_chat_room(request):
     if request.method == 'POST':
@@ -32,7 +17,10 @@ def create_chat_room(request):
         receiver = User.objects.get(pk=receiver_id)
         
         # Check if a chat room already exists between these users
-        existing_room = ChatRoom.objects.filter(user1=sender, user2=receiver).first()
+        existing_room = ChatRoom.objects.filter(
+            (Q(user1=sender) & Q(user2=receiver)) | (Q(user1=receiver) & Q(user2=sender))
+        ).first()
+
         if existing_room:
             return redirect(reverse('room', kwargs={'room_id': existing_room.id}))
         else:
