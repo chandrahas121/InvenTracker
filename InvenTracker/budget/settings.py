ff678ba5b3dbfa62e4e1feade9a8e7f5cf34cfdb
chandrahas121 <chandrahas7c@gmail.com>
2024-08-10 11:56:47 +0530

    require

--- InvenTracker/budget/settings.py
+++ InvenTracker/budget/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'daphne',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -73,9 +73,9 @@ TEMPLATES = [
     },
 ]
 
-#WSGI_APPLICATION = 'budget.wsgi.application'
+WSGI_APPLICATION = 'budget.wsgi.application'
 
-ASGI_APPLICATION = 'budget.asgi.application'
+#ASGI_APPLICATION = 'budget.asgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
